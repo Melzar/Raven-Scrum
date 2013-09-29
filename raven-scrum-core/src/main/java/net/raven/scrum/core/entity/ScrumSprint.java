a688c43fd66e3d32569f00558cc2d1d004849a1f
Melzar <tomasumat@gmail.com>
2013-09-29 22:48:55 +0200

    Add new entity scrum sprint with association

---
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/entity/ScrumSprint.java
@@ -0,0 +1,98 @@
+package net.raven.scrum.core.entity;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class ScrumSprint
+{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(insertable = false, updatable = false)
+	private long idSprint;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date endDate;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_project")
+	private ScrumProject project;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "scrum_task_in_sprint", joinColumns = { @JoinColumn(name = "id_sprint", nullable = false, updatable = false) }, inverseJoinColumns = { @JoinColumn(name = "id_task", nullable = false, updatable = false) })
+	private Set<ScrumTask> tasks;
+
+	public ScrumSprint()
+	{
+
+	}
+
+	public long getIdSprint()
+	{
+		return idSprint;
+	}
+
+	public void setIdSprint(long idSprint)
+	{
+		this.idSprint = idSprint;
+	}
+
+	public ScrumProject getProject()
+	{
+		return project;
+	}
+
+	public void setProject(ScrumProject project)
+	{
+		this.project = project;
+	}
+
+	public Set<ScrumTask> getTasks()
+	{
+		return tasks;
+	}
+
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate()
+	{
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate)
+	{
+		this.endDate = endDate;
+	}
+
+	public void setTasks(Set<ScrumTask> tasks)
+	{
+		this.tasks = tasks;
+	}
+
+}
