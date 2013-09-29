a2df62d336c5fcb1b1abfe5b63a668a6d003e668
Melzar <tomasumat@gmail.com>
2013-09-29 22:49:19 +0200

    Add new entity scrum project with associations

---
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/entity/ScrumProject.java
@@ -0,0 +1,90 @@
+package net.raven.scrum.core.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class ScrumProject
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(insertable = false, updatable = false)
+	private long idProject;
+
+	private String title;
+
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_user")
+	private ScrumUser manager;
+
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_project")
+	private Set<ScrumSprint> sprints;
+
+	public ScrumProject()
+	{
+
+	}
+
+	public long getIdProject()
+	{
+		return idProject;
+	}
+
+	public void setIdProject(long idProject)
+	{
+		this.idProject = idProject;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public ScrumUser getManager()
+	{
+		return manager;
+	}
+
+	public void setManager(ScrumUser manager)
+	{
+		this.manager = manager;
+	}
+
+	public Set<ScrumSprint> getSprints()
+	{
+		return sprints;
+	}
+
+	public void setSprints(Set<ScrumSprint> sprints)
+	{
+		this.sprints = sprints;
+	}
+
+}
