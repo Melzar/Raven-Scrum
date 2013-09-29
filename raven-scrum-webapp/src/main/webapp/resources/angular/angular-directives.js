6abe5c371d2ffe7b8df3075fb530d17010c39f5f
Melzar <tomasumat@gmail.com>
2013-09-29 22:52:36 +0200

    Add new directive for task operations

--- raven-scrum-webapp/src/main/webapp/resources/angular/angular-directives.js
+++ raven-scrum-webapp/src/main/webapp/resources/angular/angular-directives.js
@@ -70,3 +70,20 @@ scdirectives.directive('ngValidateEmail', function($http){
     }
   }
 });
+
+scdirectives.directive('ngScrumTask', function($http)
+{
+  return{
+    require : 'ngModel',
+    restrict : 'A',
+    transclude : false,
+    template: ' <div class="row"><div class="col-lg-3"><div class="subtask-type"><i class="icon-gear icon-2x"></i></div></div><div class="col-lg-7">'+
+    '<div class="row"><a class="subtask-title subtask-text text-limit-1">{{task.title}}</a></div><div class="row">'+
+    '<span class="subtask-description subtask-text text-limit-2">{{task.description}}</span></div></div><div class="col-lg-2">'+
+    '<div class="subtask-avatar pull-right"></div></div></div>',
+  //  templateUrl : '/templates/components/ScrumTask.ftl',
+    link : function(scope, element, attrs, ctrl) {
+        scope.$parent.where = attrs.ngModel
+    }
+ }
+})
