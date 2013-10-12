88ec60fe276a2867a0aa99905586a3fcbb887f0e
Melzar <tomasumat@gmail.com>
2013-10-12 16:35:53 +0200

    update angular controller with scrum operations

--- raven-scrum-webapp/src/main/webapp/resources/angular/angular-controller.js
+++ raven-scrum-webapp/src/main/webapp/resources/angular/angular-controller.js
@@ -1,4 +1,5 @@
 var sccontrollers = angular.module('scControllers', []);
+
 sccontrollers.factory('MessageData', function(){
 	return {submitsuccess: false, submiterror: false, shadowflag: 0, toggle : false}
 })
@@ -18,4 +19,36 @@ sccontrollers.controller('MessageController',function($scope, $http, $element, M
 			$scope.messagedata.submiterror = false;
 			$scope.messagedata.shadowflag = 0;
 		}
+})
+
+sccontrollers.controller('ScrumBoardController', function($q, $scope, $http, $element, ScrumData)
+{
+	$scope.scrumdata = ScrumData;
+	$http.get(ScrumData.getlink).success(function(data,status,headers,cfg){
+		$scope.scrumdata.projectdata = data;
+		$scope.scrumtasks = data.sprint.tasks;
+		$scope.setwatchers();
+	}).error(function(data,status,headers,cfg){
+		//TODO MESSAGE OF ERROR
+	})
+
+
+	$scope.setwatchers = function ()
+	{
+		 $scope.$watch(function(){ return angular.toJson($scope.scrumtasks)}, function(){
+				$scope.updatecount()
+		 })
+	}
+
+	$scope.updatecount = function()
+	{
+		$scope.scrumcounter = {todo : 0, doing : 0, uat : 0, done : 0};
+		angular.forEach($scope.scrumtasks, function(value, key){
+			$scope.scrumcounter.todo += value.todo.length;
+			$scope.scrumcounter.doing += value.doing.length;
+			$scope.scrumcounter.uat += value.uat.length;
+			$scope.scrumcounter.done += value.done.length;
+		})
+	}
+
 })
