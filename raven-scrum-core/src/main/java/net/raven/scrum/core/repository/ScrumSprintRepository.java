45413a9e37e19f8ff586cfaf32b6268007c97b13
Melzar <tomasumat@gmail.com>
2013-10-27 17:00:33 +0000

    update sprint repo

--- raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumSprintRepository.java
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumSprintRepository.java
@@ -3,15 +3,14 @@ package net.raven.scrum.core.repository;
 import net.raven.scrum.core.entity.ScrumSprint;
 import net.raven.scrum.core.enumeration.scrum.SprintStatus;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface ScrumSprintRepository extends
-		CrudRepository<ScrumSprint, Long>
+public interface ScrumSprintRepository extends JpaRepository<ScrumSprint, Long>
 {
 
-	@Query("Select distinct ss from ScrumSprint ss left join fetch ss.tasks t left join fetch ss.project p where p.idProject = :idProject and ss.status = :status")
+	@Query("Select distinct ss from ScrumSprint ss left join fetch ss.tasks t left join fetch t.subtasks sub left join fetch ss.project p left join fetch p.manager m where p.idProject = :idProject and ss.status = :status and t.parent.idTask = null")
 	public ScrumSprint getSprintData(@Param("idProject") Long idProject,
 			@Param("status") SprintStatus status);
 
