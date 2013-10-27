34ed8015e69f9922e158abbd25b78050fd1d5121
Melzar <tomasumat@gmail.com>
2013-10-27 16:43:37 +0000

    commit

--- raven-scrum-ui/src/main/java/net/raven/scrum/ui/rest/scrum/ProjectScrumboardDataResource.java
+++ raven-scrum-ui/src/main/java/net/raven/scrum/ui/rest/scrum/ProjectScrumboardDataResource.java
@@ -8,21 +8,27 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import net.raven.scrum.core.annotations.logger.Log;
+import net.raven.scrum.core.exception.ScrumException;
 import net.raven.scrum.core.rest.dto.scrum.ScrumboardDTO;
 import net.raven.scrum.ui.service.scrum.ScrumService;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/project/")
-public class ProjectScrumboardDataProvider
+public class ProjectScrumboardDataResource
 {
 
 	@Autowired
 	private ScrumService scrumService;
 
-	public ProjectScrumboardDataProvider()
+	@Log
+	private Logger log;
+
+	public ProjectScrumboardDataResource()
 	{
 
 	}
@@ -32,7 +38,31 @@ public class ProjectScrumboardDataProvider
 	@Path("/{idProject}/scrumboard/active")
 	public Response getActualSprintData(@PathParam("idProject") Long idProject)
 	{
-		ScrumboardDTO dto = scrumService.prepareDataForScrumboard(idProject);
-		return Response.status(Status.OK).entity(dto).build();
+		ScrumboardDTO dto;
+		try
+		{
+			dto = scrumService.prepareDataForScrumboard(idProject);
+			return Response.status(Status.OK).entity(dto).build();
+		} catch (ScrumException e)
+		{
+			log.error("Failed to get actual sprint data", e);
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{idProject}/users")
+	public Response getProjectUsers(@PathParam("idProject") Long idProject)
+	{
+		try
+		{
+			return Response.status(Status.OK)
+					.entity(scrumService.getProjectUsers(idProject)).build();
+		} catch (ScrumException e)
+		{
+			log.error("Failed to get users for project with id " + idProject, e);
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 }
