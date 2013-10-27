95baad1fc8790089bca7a5277455b00490b4db14
Melzar <tomasumat@gmail.com>
2013-10-27 17:05:20 +0000

    update angular controller with modal controller

--- raven-scrum-webapp/src/main/webapp/resources/angular/angular-controller.js
+++ raven-scrum-webapp/src/main/webapp/resources/angular/angular-controller.js
@@ -21,7 +21,7 @@ sccontrollers.controller('MessageController',function($scope, $http, $element, M
 		}
 })
 
-sccontrollers.controller('ScrumBoardController', function($q, $scope, $http, $element, ScrumData)
+sccontrollers.controller('ScrumBoardController', function($scope, $http, $element, ScrumData)
 {
 	$scope.scrumdata = ScrumData;
 	$http.get(ScrumData.getlink).success(function(data,status,headers,cfg){
@@ -44,11 +44,89 @@ sccontrollers.controller('ScrumBoardController', function($q, $scope, $http, $el
 	{
 		$scope.scrumcounter = {todo : 0, doing : 0, uat : 0, done : 0};
 		angular.forEach($scope.scrumtasks, function(value, key){
-			$scope.scrumcounter.todo += value.todo.length;
-			$scope.scrumcounter.doing += value.doing.length;
-			$scope.scrumcounter.uat += value.uat.length;
-			$scope.scrumcounter.done += value.done.length;
+			console.log(value)
+			$scope.scrumcounter.todo += value.progress.TODO.length;
+			$scope.scrumcounter.doing += value.progress.DOING.length;
+			$scope.scrumcounter.uat += value.progress.UAT.length;
+			$scope.scrumcounter.done += value.progress.DONE.length;
 		})
 	}
+})
+
+sccontrollers.controller('ModalController', function($scope, $http, $modal, $log, TemplateData)
+{
+
+	$scope.addTask = function()
+	{
+		TemplateData.select2tasks.data.results = $scope.scrumtasks;
+		TemplateData.modalbody = TemplateData.sourcelink + '/template/modal/ScrumModalBody.ftl';
+		TemplateData.modaltitle = "Add Task";
+
+		$http.get(TemplateData.sourcelink + '/rest/project/1/users').success(function(data,status,headers,cfg){
+			TemplateData.select2users.data.results = data;
+		}).error(function(data,status,headers,cfg){
+		//TODO MESSAGE OF ERROR
+		})
+
+		$http.get(TemplateData.sourcelink + "/rest/task/type").success(function(data, status, headers, cfg){
+			TemplateData.select2types.data.results = data;
+		}).error(function(data,status,headers,cfg){
+		//TODO ERROR MESSAGE
+		})	
+
+		var modalInstance = $modal.open(
+		{
+			templateUrl: TemplateData.sourcelink + '/template/modal/ModalTemplate.ftl',
+      		controller: 'ModalInstanceController',
+      		resolve: {
+        		data: function () {
+        		  return TemplateData;
+        		}
+      		}
+		})
+
+		modalInstance.result.then(function (promiseditem) {
+    	}, function (promiseditem) {
+    		if(promiseditem)
+    		{
+    			console.log($scope)
+    		}
+    	  	$log.info('Modal dismissed at: ' + new Date());
+    	});
+	}
+})
+
+sccontrollers.controller('ModalInstanceController', function($scope, $http, $modalInstance, data)
+{
+	$scope.data = data;
+	$scope.closeModal = function()
+	{
+		$modalInstance.dismiss();
+	}
+
+	$scope.save = function()
+	{
+			var postdata = {idUser: "", idParent: "", description: "", type: ""};
+			postdata.idUser = data.select2user.id;
+			postdata.idParent = data.select2task.id;
+			postdata.description = data.taskdescription;
+			postdata.type = data.select2type.id;
+			$http({
+				url: data.sourcelink + "/rest/task/" + data.select2task.id + "/add",
+				method: "POST",
+				data: postdata ,
+				headers : {'Content-Type': 'application/json'}
+			}).success(function(datares, status, headers, cfg){
+				data.select2task.progress.TODO.push(datares);
+				$modalInstance.close(datares);
+			}).error(function(datares, status, headers, cfg){
+				//todo messagebox
+				console.log("error")
+			})
+	}
+})
+
+sccontrollers.controller('NavigationController', function($scope, $http)
+{
 
 })
