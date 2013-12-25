60a02f1ae1529f7e1319c8a4055f1153900c67ab
Melzar <tomasumat@gmail.com>
2013-12-26 00:16:46 +0100

    Update repository to new schema of model

--- raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumProjectRepository.java
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumProjectRepository.java
@@ -4,14 +4,14 @@ import java.util.Collection;
 
 import net.raven.scrum.core.entity.ScrumProject;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ScrumProjectRepository extends
-		CrudRepository<ScrumProject, Long>
+		JpaRepository<ScrumProject, Long>
 {
-	@Query("from ScrumProject p left join fetch p.projectUsers pu where pu.login = :login ")
+	@Query("from ScrumProject p left join fetch p.userprojectrole pu where pu.pk.user.login = :login ")
 	public Collection<ScrumProject> getProjectListForUser(
 			@Param("login") String login);
 
