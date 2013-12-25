00485009807154bd5ec399f4a04dea6fdd24c04e
Melzar <tomasumat@gmail.com>
2013-12-26 00:19:49 +0100

    Add role to query

--- raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumUserRepository.java
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumUserRepository.java
@@ -4,11 +4,11 @@ import java.util.Collection;
 
 import net.raven.scrum.core.entity.ScrumUser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface ScrumUserRepository extends CrudRepository<ScrumUser, Long>
+public interface ScrumUserRepository extends JpaRepository<ScrumUser, Long>
 {
 	@Query("from ScrumUser u where u.login = :login")
 	public ScrumUser getUserByLogin(@Param("login") String login);
@@ -22,7 +22,7 @@ public interface ScrumUserRepository extends CrudRepository<ScrumUser, Long>
 	@Query("select case when (count(u) > 0) then false else true end from ScrumUser u where u.login = :login")
 	public boolean isLoginUnique(@Param("login") String login);
 
-	@Query("from ScrumUser u left join fetch u.projects p where p.id = :id and u.shadowFlag = 0")
+	@Query("from ScrumUser u left join fetch u.userprojectrole p where p.pk.project.id = :id and u.shadowFlag = 0")
 	public Collection<ScrumUser> getUsersFromProject(@Param("id") Long id);
 
 	@Query("from ScrumUser u where u.shadowFlag = 0")
