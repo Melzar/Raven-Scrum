f7eef17c469c78254b7907ffe062915064633692
Matuszewski <fake@email.com>
2014-02-16 19:14:06 +0100

    update project repository with two methods : checking if title of project is unique and getting project with users

--- raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumProjectRepository.java
+++ raven-scrum-core/src/main/java/net/raven/scrum/core/repository/ScrumProjectRepository.java
@@ -11,8 +11,14 @@ import org.springframework.data.repository.query.Param;
 public interface ScrumProjectRepository extends
 		JpaRepository<ScrumProject, Long>
 {
-	@Query("from ScrumProject p left join fetch p.userprojectrole pu where pu.pk.user.login = :login ")
+	@Query("from ScrumProject p join fetch p.userprojectrole pu where pu.pk.user.login = :login ")
 	public Collection<ScrumProject> getProjectListForUser(
 			@Param("login") String login);
+
+	@Query("select case when (count (p) > 0) then false else true end from ScrumProject p where p.title = :title")
+	public boolean isProjectTitleUnique(@Param("title") String title);
+
+	@Query("from ScrumProject p left outer join fetch p.userprojectrole where p.idProject = :idProject")
+	public ScrumProject getProjectWithUsers(@Param("idProject") long idProject);
 
 }
